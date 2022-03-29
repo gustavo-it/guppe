71ba9618031d7e38a0d70684ca64c36de38ce48e
gusta-silva <ggustavo2216@gmail.com>
2022-03-29 11:26:44 -0300

    tipo numérico

---
+++ tipo_numerico.py
@@ -0,0 +1,16 @@
+"""
+Tipo numérico
+
+5 // 2 -> irá nos devolver um número inteiro e não float
+
+em python o limite de um número inteiro que podemos armazenar depende da memória do computador
+
+num = 45
+
+num += 2 -> é o mesmo que num = num + 2, ou seja, estou somando um valor a variável
+
+posso utilizar valores maiores da seguinte forma: 1_000_00 (separando por underline)
+
+"""
+num = 1_000_00
+print(num)
