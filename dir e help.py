cca1d96e93643c43cda1c59c6b4570b02c7d539f
gusta-silva <ggustavo2216@gmail.com>
2022-03-29 10:03:34 -0300

    dir and help

---
+++ dir e help.py
@@ -0,0 +1,18 @@
+""""
+Utilitários Python para te axuliar na programação.
+
+Dir -> Apresenta todos os atributos/propriedades e funções/métodos disponíveis para
+determinado tipo de dado ou variável.
+
+dir(tipo de dado/variável) = apresenta todos os métodos, funções disponíveis para esse dado/variável
+
+Help(saber o que faz) -> Apresenta a documentação/como utilizar os atributos/propriedades e funções/métodos disponíveis para
+determinado tipo de dado ou variável.
+
+help(dado/variável.propriedade)
+
+caso queira analisar um número:
+num = 4
+help(num.propriedade)
+
+"""
