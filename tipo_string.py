f6e53a0a68b16311ff53beb3f0096df677d6451b
megusta2 <gustavo_castrosilva@yahoo.com>
2022-03-31 15:07:10 -0300

    tipo string

---
+++ tipo_string.py
@@ -0,0 +1,27 @@
+"""
+Tipo String
+
+em python, string é tudo que estiver entre:
+- Aspas simples;
+- Aspas duplas;
+- Aspas simples duplas;
+- Aspas duplas triplas;
+
+- Aspas simples -> 'nome';
+- Aspas duplas -> "nome";
+- Aspas simples duplas -> ''nome''
+
+print(nome.split()) # transforma em uma lista de strings
+"""
+
+nome = "Geek University"
+
+print(nome[0:4])
+# slice de tring
+
+print(nome[5:15])
+# Slice de tring
+
+
+
+
